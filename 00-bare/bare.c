799c7d7cafebf79583a9f78a7291a84e1f664921
En Shih <seanstone5923@gmail.com>
2018-04-15 15:23:15 +0800

    Fixes

--- 00-bare/bare.c
+++ 00-bare/bare.c
@@ -4,10 +4,3 @@ void main(void)
 {
 	while (1) ;
 }
-
-__attribute((section(".isr_vector"))) __attribute__((used))
-uint32_t *isr_vectors[] =
-{
-	0,
-	(uint32_t *) Reset_Handler,	/* code entry point */
-};
