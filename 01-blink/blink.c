046da03c392725d30b6c229dfd11caf02657199c
En Shih <seanstone5923@gmail.com>
2018-04-20 21:28:30 +0800

    Fix 01-blink

--- 01-blink/blink.c
+++ 01-blink/blink.c
@@ -1,8 +1,13 @@
+#include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
 int main (void)
 {
+    /* Enable GPIOA clock. */
+    rcc_periph_clock_enable(RCC_GPIOA);
+
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
+    
     while (1)
     {
         gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
