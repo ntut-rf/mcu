80b8a15546b768a68fa7a3ed069b53cc25698c76
En Shih <seanstone5923@gmail.com>
2018-04-21 19:55:09 +0800

    Read Accel_z

--- 04-spi-mpu9250/main.c
+++ 04-spi-mpu9250/main.c
@@ -12,6 +12,22 @@ int main (void)
     rcc_clock_setup_in_hse_8mhz_out_72mhz();
     usart_setup();
     MPU9250_setup_SPI();
-    for (int i = 0; i <= 0x7E; i++)
-        printf("0x%02x: 0x%02x\r\n", i, MPU9250_read(i));
+    printf("\r\n");
+    printf("MPU9250 init\r\n");
+    printf("MPU9250_WHO_AM_I: 0x%02x\r\n", MPU9250_read(MPU9250_WHO_AM_I));
+    while (1)
+    {
+        int32_t sum;
+        int32_t samples;
+
+        sum += (int16_t) (MPU9250_read(MPU9250_ACCEL_ZOUT_H) << 8 | MPU9250_read(MPU9250_ACCEL_ZOUT_L));
+        samples ++;
+
+        if (samples >= 5000)
+        {
+            printf("MPU9250_ACCEL_ZOUT: %f g\r\n", (float) sum / samples / 16384);
+            sum = 0;
+            samples = 0;
+        }
+    }
 }
